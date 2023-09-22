eec91f07ed505b925cccfff5110e5ea783a6d264
SystemSl <144921270+SystemSl@users.noreply.github.com>
2023-09-22 18:19:28 +0400

    Object name change

--- Lab1/Task4/MyFirstProgram.java
+++ Lab1/Task4/MyFirstProgram.java
@@ -2,13 +2,13 @@ import myfirstpackage.*;
 
 class MyFirstClass {
 	public static void main(String[] s) {
-		MySecondClass Numbers = new MySecondClass(1, 2);
-		System.out.println(Numbers.RemainderOfTheDivision());
+		MySecondClass o = new MySecondClass(1, 2);
+		System.out.println(o.RemainderOfTheDivision());
 		for (int i = 1; i <= 8; i++) {
 			for (int j = 1; j <= 8; j++) {
-			Numbers.SetArg1(i);
-			Numbers.SetArg2(j);
-			System.out.print(Numbers.RemainderOfTheDivision());
+			o.SetArg1(i);
+			o.SetArg2(j);
+			System.out.print(o.RemainderOfTheDivision());
 			System.out.print(" ");
 			}
 		System.out.println();
