9b1f2af9c2d9611be74fe1e69dc696dfd009f915
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-09-30 15:50:36 +0400

    SqrFunction class with test class

---
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/SqrFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.systemsl.sandbox.Lab2.functions;
+
+public class SqrFunction implements MathFunction{
+    public double apply(double x) {
+        return Math.pow(x, 2);
+    }
+}
---
+++ src/test/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/SqrFunctionTest.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.systemsl.sandbox.Lab2.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqrFunctionTest {
+
+    @Test
+    void apply() {
+        SqrFunction a = new SqrFunction();
+        assertEquals(a.apply(2),4);
+    }
+}
