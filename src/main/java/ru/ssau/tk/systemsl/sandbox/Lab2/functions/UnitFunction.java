69c71b1729059c5bacfeb664d3d04f8216d4d907
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-10-01 18:06:12 +0400

    UnitFunction and ZeroFunction fix

--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/UnitFunction.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/UnitFunction.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk.systemsl.sandbox.Lab2.functions;
 
-public class UnitFunction extends ConstantFunction{
+public class UnitFunction extends ConstantFunction implements MathFunction{
     public UnitFunction() {
         super(1);
     }
--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/ZeroFunction.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/ZeroFunction.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk.systemsl.sandbox.Lab2.functions;
 
-public class ZeroFunction extends ConstantFunction{
+public class ZeroFunction extends ConstantFunction implements MathFunction{
     public ZeroFunction() {
         super(0);
     }
