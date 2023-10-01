b61a9684be0c66fc375a39a4a26ebe941fa41168
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-10-01 18:08:24 +0400

    SumAsinAcosFunction fix

--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/SumAsinAcosFunction.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/SumAsinAcosFunction.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk.systemsl.sandbox.Lab2.functions;
 
-public class SumAsinAcosFunction {
+public class SumAsinAcosFunction implements MathFunction{
     public double apply(double x) {
         return Math.acos(x) + Math.asin(x);
     }
