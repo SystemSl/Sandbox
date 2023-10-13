976b68fef502ed434fc9343b8c7eb3bdce23d4b7
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-10-13 15:31:08 +0400

    toString, equals, hashCode, clone fo IdentityFunction

--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/IdentityFunction.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/IdentityFunction.java
@@ -1,7 +1,23 @@
 package ru.ssau.tk.systemsl.sandbox.Lab2.functions;
 
-public class IdentityFunction implements MathFunction {
+public class IdentityFunction implements MathFunction, Cloneable {
     public double apply (double x) {
         return x;
+    }
+
+    public String toString() {
+        return "IdentityFunction";
+    }
+
+    public boolean equals(Object o) {
+        return (this.getClass() == o.getClass());
+    }
+
+    public int hashCode() {
+        return this.getClass().hashCode();
+    }
+    @Override
+    public Object clone() {
+        return new IdentityFunction();
     }
 }
--- src/test/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/IdentityFunctionTest.java
+++ src/test/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/IdentityFunctionTest.java
@@ -9,4 +9,32 @@ class IdentityFunctionTest {
         IdentityFunction x = new IdentityFunction();
         assertEquals(x.apply(10), 10);
     }
+
+    @org.junit.jupiter.api.Test
+    void tS() {
+        IdentityFunction x = new IdentityFunction();
+        assertEquals(x.toString(), "IdentityFunction");
+    }
+
+    @org.junit.jupiter.api.Test
+    void cl() {
+        IdentityFunction x = new IdentityFunction();
+        IdentityFunction y = (IdentityFunction) x.clone();
+        assertEquals(y.apply(10), 10);
+    }
+
+    @org.junit.jupiter.api.Test
+    void eq() {
+        IdentityFunction x = new IdentityFunction();
+        IdentityFunction y = (IdentityFunction) x.clone();
+        assertEquals(x, y);
+        assertEquals(y, x);
+    }
+
+    @org.junit.jupiter.api.Test
+    void hC() {
+        IdentityFunction x = new IdentityFunction();
+        IdentityFunction y = (IdentityFunction) x.clone();
+        assertEquals(x.hashCode(), y.hashCode());
+    }
 }
