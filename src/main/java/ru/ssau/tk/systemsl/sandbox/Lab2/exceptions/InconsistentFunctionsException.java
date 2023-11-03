7c07b1ff36a4cc411ccc8a5b8e3073feecacc102
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-11-03 23:31:29 +0400

    InconsistentException

---
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.systemsl.sandbox.Lab2.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException{
+    public InconsistentFunctionsException() {}
+    public InconsistentFunctionsException(String msg) {
+        super(msg);
+    }
+}
