caadec54b1a0eee952cd1b03d5bc4e31fee4efcf
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-11-04 20:00:45 +0400

    TabulatedFunctionFileReader + readTabulatedFunction method

--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/io/FunctionsIO.java
@@ -1,6 +1,12 @@
 package ru.ssau.tk.systemsl.sandbox.Lab2.io;
 import java.io.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
 import ru.ssau.tk.systemsl.sandbox.Lab2.functions.*;
+import ru.ssau.tk.systemsl.sandbox.Lab2.functions.factory.*;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
@@ -12,5 +18,36 @@ public final class FunctionsIO {
             printwriter.printf("%f %f\n", point.x, point.y);
         }
         printwriter.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = 0;
+        try {
+            count = Integer.parseInt(reader.readLine());
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat nf = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < count; i++) {
+            String xy = "";
+            try {
+                xy = reader.readLine();
+            }
+            catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+            String[] xay = xy.split(" ");
+            try {
+                xValues[i] = nf.parse(xay[0]).doubleValue();
+                yValues[i] = nf.parse(xay[1]).doubleValue();
+            }
+            catch (ParseException pe) {
+                throw new IOException(pe);
+            }
+        }
+        return factory.create(xValues, yValues);
     }
 }
