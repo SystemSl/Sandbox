3b629f02b52b5baad93dfa173a8ab59ccebb5956
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-11-04 22:18:53 +0400

    Preparing to serializing

--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/functions/AbstractTabulatedFunction.java
@@ -3,7 +3,14 @@ package ru.ssau.tk.systemsl.sandbox.Lab2.functions;
 import ru.ssau.tk.systemsl.sandbox.Lab2.exceptions.ArrayIsNotSortedException;
 import ru.ssau.tk.systemsl.sandbox.Lab2.exceptions.DifferentLengthOfArraysException;
 
-public abstract class AbstractTabulatedFunction implements TabulatedFunction{
+import java.io.Serial;
+import java.io.Serializable;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction, Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -6030744339129575121L;
+
     protected abstract int floorIndexOfX(double x);
     protected abstract double extrapolateLeft(double x);
     protected abstract double extrapolateRight(double x);
