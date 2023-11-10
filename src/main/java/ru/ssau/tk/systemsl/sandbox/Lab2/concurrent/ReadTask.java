ebca91e09b2e102968975d5986927e9db4f6c836
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-11-10 19:56:41 +0400

    ReadTask

---
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/concurrent/ReadTask.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.systemsl.sandbox.Lab2.concurrent;
+
+import ru.ssau.tk.systemsl.sandbox.Lab2.functions.TabulatedFunction;
+
+public class ReadTask implements Runnable{
+    private final TabulatedFunction function;
+    public ReadTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            System.out.printf("After read: i = %d, x = %f, y = %f", i, function.getX(i), function.getY(i));
+        }
+    }
+}
