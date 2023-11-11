cf112fbc63b9af6050432b983bf4040e08827996
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-11-11 15:11:47 +0400

    DeriveSynchronously method with tests

--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/operations/TabulatedDifferentialOperator.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/operations/TabulatedDifferentialOperator.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.systemsl.sandbox.Lab2.operations;
 
+import ru.ssau.tk.systemsl.sandbox.Lab2.concurrent.SynchronizedTabulatedFunction;
 import ru.ssau.tk.systemsl.sandbox.Lab2.functions.*;
 import ru.ssau.tk.systemsl.sandbox.Lab2.functions.factory.*;
 
@@ -40,5 +41,31 @@ public class TabulatedDifferentialOperator implements DifferentialOperator<Tabul
         yValues[i] = yValues[i - 1];
 
         return factory.create(xValues, yValues);
+    }
+    public SynchronizedTabulatedFunction deriveSynchronously(TabulatedFunction function) {
+        SynchronizedTabulatedFunction syn_function;
+        if (function.getClass() != SynchronizedTabulatedFunction.class)
+            syn_function = new SynchronizedTabulatedFunction(function);
+        else
+            syn_function = (SynchronizedTabulatedFunction) function;
+        SynchronizedTabulatedFunction.Operation<SynchronizedTabulatedFunction> sync_derive = syn_func -> {
+            int count = syn_func.getCount();
+            Point[] points = TabulatedFunctionOperationService.asPoints(syn_func);
+            double[] xValues = new double[count];
+            double[] yValues = new double[count];
+
+            int i = 0;
+            while (i < count - 1) {
+                xValues[i] = points[i].x;
+                yValues[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
+                i++;
+            }
+
+            xValues[i] = points[i].x;
+            yValues[i] = yValues[i - 1];
+
+            return new SynchronizedTabulatedFunction(factory.create(xValues, yValues));
+        };
+        return syn_function.doSynchronously(sync_derive);
     }
 }
--- src/test/java/ru/ssau/tk/systemsl/sandbox/Lab2/operations/TabulatedDifferentialOperatorTest.java
+++ src/test/java/ru/ssau/tk/systemsl/sandbox/Lab2/operations/TabulatedDifferentialOperatorTest.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.systemsl.sandbox.Lab2.operations;
 
 import org.junit.jupiter.api.Test;
+import ru.ssau.tk.systemsl.sandbox.Lab2.concurrent.SynchronizedTabulatedFunction;
 import ru.ssau.tk.systemsl.sandbox.Lab2.functions.*;
 import ru.ssau.tk.systemsl.sandbox.Lab2.functions.factory.*;
 
@@ -51,5 +52,20 @@ class TabulatedDifferentialOperatorTest {
         assertEquals(differentialFunction.getY(1), 3.0);
         assertEquals(differentialFunction.getY(2), 5.0);
         assertEquals(differentialFunction.getY(3), 5.0);
+    }
+
+    @Test
+    void deriveSynchronouslyTest() {
+        TabulatedDifferentialOperator differentialOperator = new TabulatedDifferentialOperator(new ArrayTabulatedFunctionFactory());
+        ArrayTabulatedFunction array = new ArrayTabulatedFunction(new UnitFunction(), 1, 1000, 1000);
+        SynchronizedTabulatedFunction syn_derived_array1 = differentialOperator.deriveSynchronously(array);
+        SynchronizedTabulatedFunction syn_array = new SynchronizedTabulatedFunction(array);
+        SynchronizedTabulatedFunction syn_derived_array2 = differentialOperator.deriveSynchronously(syn_array);
+        for (Point p : syn_derived_array1) {
+            assertEquals(0, p.y);
+        }
+        for (Point p : syn_derived_array2) {
+            assertEquals(0, p.y);
+        }
     }
 }
