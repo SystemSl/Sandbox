0db7c016ae6498ac60f1e649051075c7ebfd3f53
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-11-16 23:26:44 +0400

    Fix

--- src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/concurrent/IntegrateSectorTask.java
+++ src/main/java/ru/ssau/tk/systemsl/sandbox/Lab2/concurrent/IntegrateSectorTask.java
@@ -14,14 +14,12 @@ public class IntegrateSectorTask implements Callable<Double> {
         this.x2 = x2;
     }
     public Double call() {
-        double result = 0;
         double y1 = function.getY(function.indexOfX(x1));
         double y2 = function.getY(function.indexOfX(x2));
         if ((x1 >= 0) != (x2 >=0)) {
             double x0 = (-y1/(y2-y1))*(x2-x1)+x1;
             return (Math.abs((y1/2)*(x2-x1)) + Math.abs((y2/2)*(x2-x1)));
         }
-        result = Math.abs(((y1+y2)/2)*(x2-x1));
-        return result;
+        return Math.abs(((y1+y2)/2)*(x2-x1));
     }
 }
