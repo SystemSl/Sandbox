f4a32032d5ba03afb1c5aa5b26ce17b109ee9fbf
Sulimov <Dmitriy.sulimov2003@gmail.com>
2023-11-27 12:31:15 +0400

    test

--- src/test/java/ru/ssau/tk/systemsl/sandbox/ApplicationTests.java
+++ src/test/java/ru/ssau/tk/systemsl/sandbox/ApplicationTests.java
@@ -2,8 +2,9 @@ package ru.ssau.tk.systemsl.sandbox;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.ssau.tk.systemsl.sandbox.Lab2.Application;
 
-@SpringBootTest
+@SpringBootTest(classes=Application.class)
 class ApplicationTests {
 
 	@Test
